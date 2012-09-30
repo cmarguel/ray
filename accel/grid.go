a14dec814b1da17074bd3b5d9944c2f2cc3f919f
Miguel Arguelles <cmarguel@gmail.com>
2012-09-30 16:35:23 +0800

    initialize primitives with primitives for grid

--- accel/grid.go
+++ accel/grid.go
@@ -5,10 +5,21 @@ import (
 )
 
 type Grid struct {
+	primitives *list.List
 }
 
 func NewGrid(p *list.List, refineImmediately bool) Grid {
 	// initialize primitives
+	var primitives *list.List = nil
+
+	if refineImmediately {
+		primitives = list.New()
+		for e := p.Front(); e != nil; e = e.Next() {
+			FullyRefine(e.Value.(Primitive), primitives)
+		}
+	} else {
+		primitives = p
+	}
 	// compute bounds, choose grid res
 	// compute voxel widths, allocate voxels
 	// add primitives
