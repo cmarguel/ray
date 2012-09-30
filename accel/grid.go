93a8795deee938bb368df1c3d752dd6f7e1757d0
Miguel Arguelles <cmarguel@gmail.com>
2012-09-30 16:28:07 +0800

    added stub for grid

--- accel/grid.go
+++ accel/grid.go
@@ -1,1 +1,18 @@
 package accel
+
+import (
+	"container/list"
+)
+
+type Grid struct {
+}
+
+func NewGrid(p *list.List, refineImmediately bool) Grid {
+	// initialize primitives
+	// compute bounds, choose grid res
+	// compute voxel widths, allocate voxels
+	// add primitives
+	// create reader-writer mutex
+
+	return *new(Grid)
+}
