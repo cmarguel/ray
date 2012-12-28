fc2ad762e244b70e9100ea1005ad56e7a22ec821
Miguel Arguelles <cmarguel@gmail.com>
2012-12-28 22:29:52 +0800

    projective camera

---
+++ camera/projective.go
@@ -0,0 +1,36 @@
+package camera
+
+import (
+	"ray/camera/film"
+	"ray/geom"
+	"ray/mmath"
+	"ray/render/sampler"
+)
+
+type Projective struct {
+	CameraToScreen mmath.Transform
+	RasterToCamera mmath.Transform
+	CameraToWorld  mmath.Transform
+	ScreenToRaster mmath.Transform
+	RasterToScreen mmath.Transform
+
+	film film.ImageFilm
+}
+
+func NewProjective(c2w, proj mmath.Transform, window []float64, film film.ImageFilm) Projective {
+	c2s := proj
+
+	s2r := mmath.NewTransform().
+		Translate(-window[0], -window[3], 0.).
+		Scale(1./(window[1]-window[0]), 1./(window[2]-window[3]), 1.).
+		Scale(float64(film.ResolutionX), float64(film.ResolutionY), 1.)
+
+	r2s := s2r.Inverse()
+
+	r2c := c2s.Inverse().Times(r2s)
+	return Projective{c2s, r2c, c2w, s2r, r2s, film}
+}
+
+func (p Projective) Film() film.Film {
+	return p.film
+}
